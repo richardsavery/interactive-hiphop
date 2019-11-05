da6df42369a504b863dfb182cceb7690e3a130a8
Spencer <spencersnotgold@gmail.com>
2019-11-05 11:13:52 -0500

    Create timer for each module

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
 import json
+from time import time
 
+import_start_time = time()
 from speech_to_text.transcriber import SpeechToText
 from rhymes_keywords_sentiment.keywordextraction import KeywordExtractor
 from rhymes_keywords_sentiment.sentiment_analysis import SentimentAnalyzer
 from lyric_generation.lstm import LSTM_Generator
 from markov_model_rhymes.markov_rhyme import MarkovRhymeGenerator
-from text_to_rhythm.text_to_speech import text_to_rhythm
+# from text_to_rhythm.text_to_speech import text_to_rhythm
+import_total_time = time() - import_start_time
+print("Time for all imports:", import_total_time)
 
 def print_spacer(text):
     print(f"""\n\n\n
@@ -23,40 +27,47 @@ def main():
 
 
     # get input lyrics text from audio
-    # print_spacer("Speech To Text")
-    # audio_path = os.path.join(data_directory, "sample1.wav")
-    # speech_to_text = SpeechToText()
-    # input_lyrics_file_path = speech_to_text.transcribe_audio(audio_path)
-
-    input_lyrics_file_path = os.path.join(project_directory, 'speech_to_text', 'sample1.txt')
+    print_spacer("Speech To Text")
+    speech_to_text_start = time()
+    audio_path = os.path.join(data_directory, "sample1.wav")
+    speech_to_text = SpeechToText()
+    input_lyrics_file_path = speech_to_text.transcribe_audio(audio_path)
 
     with open(input_lyrics_file_path) as f:
         input_lyrics = f.read().lower()
 
-    # get keywords from text
-    # print_spacer("Keyword Extraction")
-    # keyword_extractor = KeywordExtractor(model="textrank")
-    # extra_stop_words = ["n't", "'s", "'m", "``", "'", '"', '.', ","]
-    # keyword_extractor.set_stopwords(extra_stop_words)
+    speech_to_text_total_time = time() - speech_to_text_start
+    print('Speech to text time:', speech_to_text_total_time)
 
-    # keyword_extractor.analyze(input_lyrics)
-    # keywords = keyword_extractor.get_keywords(5)
-    # print('keywords:', keywords)
-    # keywords[0] = 'robot'
-    keywords = ['robot']
+
+    # get keywords from text
+    keyword_start_time = time()
+    print_spacer("Keyword Extraction")
+    keyword_extractor = KeywordExtractor(model="textrank")
+    extra_stop_words = ["n't", "'s", "'m", "``", "'", '"', '.', ","]
+    keyword_extractor.set_stopwords(extra_stop_words)
+
+    keyword_extractor.analyze(input_lyrics)
+    keywords = keyword_extractor.get_keywords(5)
+    print('keywords:', keywords)
+    keyword_total_time = time() - keyword_start_time
+    print("Keyword time: ", keyword_total_time)
 
     # generate lyrics
-    # print_spacer("Lyric Generation")
-    # generator = LSTM_Generator()
-    # output_lyrics = generator.generate(keywords[0])
-    # output_lyrics = output_lyrics.replace('nigga', 'dude')
+    generation_start_time = time()
+    print_spacer("Lyric Generation")
+    generator = LSTM_Generator()
+    output_lyrics = generator.generate(keywords[0])
 
-    output_lyrics = "dro endline got jones scars and shots strap out and beat endline taking the microphone man's light for endline so often takin' around or this clothes what they sprinting nigga feeling endline and i wasn't learning she vote we still talking endline i'm on my person i got my own shit endline i'm on ya' own to me 'cause the baby of white mess endline cause i'm living to fuck with me my dollar though endline anybody blow with no tag nigga endline and other boy don't let you grow endline look into a jeep and a wild skin endline all you niggas got pushed and they fuckin' smoke endline and before we come for tomorrow who i know endline i should be smokin' up then andre hit 'em girl i touched them bogus display broken off endline better and all of drug one man 44 and they threw around and endline he said he don't know about it endline 'cause when i actually come on all endline i remember the strap off then then in us endline before i was the shame i got a new life on the sky endline and all i could quit mad g's on me endline it's the new dealer endline who you made me some shit i was a paid endline your momma natural sweet to all endline for my room that they know not it endline i got my own direction endline and i don't know that nigga endline let you pull the lab and go out of chat that endline pull up in my house like a family on the track endline and i don't know that i know endline endverse"
+    generation_total_time = time() - generation_start_time
+    print("generation time: ", generation_total_time)
+
 
     print_spacer("Generated Lyrics")
     print(output_lyrics.replace('endline', '\n'))
 
     # generate rhymes
+    rhymes_start_time = time()
     print_spacer("Rhyme Generation")
     output_lyrics = output_lyrics.replace('endverse', '')
     output_lines = output_lyrics.split('endline')[:5]
@@ -71,39 +82,33 @@ def main():
         if rhyming_line:
             print('\n****found rhyme****')
             print("Initial line:", line)
-            print("Rhyming line:", rhyming_line.replace('nigga', 'dude'))
             output_lines_with_rhymes.append(rhyming_line)
+
+    rhymes_total_time = time() - rhymes_start_time
+    print("rhymes time: ", rhymes_total_time)
 
 
     output_lyrics_with_rhymes = "\n".join(output_lines_with_rhymes)
-    output_lines_with_rhymes = output_lyrics_with_rhymes.replace('nigga', 'dude')
-
-    # output_lyrics_with_rhymes = """
-    # got jones scars and shots strap out and beat
-
-    # thanks!" this fly flow take practice like tae bo with billy blanks to get the paper on the caper heard me in the benz in the backseat
-    
-    # taking the microphone man's light for
-    # so often takin' around or this clothes what they sprinting nigga feeling
-
-    # y'all!) (can i kick it?) as you inhale like a breath of fresh air the game's a pound of that bay area kush it's nothin' we can do this shit right here in front of our building yeah, further, ice and glamorized drug dealing was appealing
-    # and i wasn't learning she vote we still talking
-
-    # thanks!" this fly flow take practice like tae bo with billy blanks to get the loot so i can tell him how much i fucking hate you making my eyes ache, stalking
-    # """
 
     print("\n\nlyrics with rhymes:")
     print(output_lyrics_with_rhymes)
 
-    # # get sentiment from text
-    # print_spacer("Sentiment Analysis")
-    # sentiment_analyzer = SentimentAnalyzer()
-    # neg_score = sentiment_analyzer.get_negativity(output_lyrics_with_rhymes)
-    # print('aggresion score =', neg_score)
+    # get sentiment from text
+    sentiment_start_time = time()
+    print_spacer("Sentiment Analysis")
+    sentiment_analyzer = SentimentAnalyzer()
+    neg_score = sentiment_analyzer.get_negativity(output_lyrics_with_rhymes)
+    print('aggresion score =', neg_score)
+
+    sentiment_total_time = time() - sentiment_start_time
+    print("sentiment time: ", sentiment_total_time)
     
 
     # say the words
-    text_to_rhythm(output_lyrics_with_rhymes, 101)
+    text_to_rhythm_start_time = time()
+    # text_to_rhythm(output_lyrics_with_rhymes, 101)
+    text_to_rhythm_total_time = time() - text_to_rhythm_start_time
+    print("text_to_rhythm time: ", text_to_rhythm_total_time)
 
 def json_to_corpus(fname):
     with open(fname, 'r') as f:
